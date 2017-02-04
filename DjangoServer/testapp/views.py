be964f39d9e4ab22b0a82b5e38f2cc095841f0d0
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-02-05 01:54:44 +0900

    no message

--- DjangoServer/testapp/views.py
+++ DjangoServer/testapp/views.py
@@ -10,10 +10,6 @@ from testapp.serializers import TestSerializer
 
 
 class TestViewSet(viewsets.ModelViewSet):
-    """
-    이 뷰셋은 `list`와 `create`, `retrieve`, `update`, 'destroy` 기능을 자동으로 지원합니다
 
-    여기에 `highlight` 기능의 코드만 추가로 작성했습니다
-    """
     queryset = Test.objects.all()
     serializer_class = TestSerializer
