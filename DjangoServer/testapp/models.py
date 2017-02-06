6a61a66632b37fff19a800f142f6f0f1ef8907ce
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-02-06 15:39:21 +0900

    Authentication & Permissions

--- DjangoServer/testapp/models.py
+++ DjangoServer/testapp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
-# Create your models here.
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
 class Test(models.Model):
     id = models.AutoField(primary_key=True)
     content = models.CharField(max_length=100, default="Hello, World!")
+    owner = models.ForeignKey('auth.User', related_name='testapp')
 
     class Meta:
         ordering = ('id',)
