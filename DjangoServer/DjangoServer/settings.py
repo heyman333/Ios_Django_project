2f467a259e394f35afbbf819ad9cfeb7d0460520
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-02-06 23:08:19 +0900

    MySQL

--- DjangoServer/DjangoServer/settings.py
+++ DjangoServer/DjangoServer/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -80,8 +83,12 @@ WSGI_APPLICATION = 'DjangoServer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ios_django',
+        'USER': 'root',
+        'PASSWORD': '999999999',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
