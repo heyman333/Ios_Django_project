72e9c0ae4c50ea6c0506ac419ab9158fc0271b78
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-02-14 18:32:33 +0900

    token

--- DjangoServer/users/urls.py
+++ DjangoServer/users/urls.py
@@ -12,7 +12,6 @@ router.register(r'profile', views.ProfileViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^register/', UserCreateAPIView.as_view(), name='register'),
     url(r'^login/', UserLoginAPIView.as_view(), name='login'),
     url(r'^auth/token/', obtain_jwt_token),
 ]
