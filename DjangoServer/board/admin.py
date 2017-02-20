9d28d35c0427b3c638450496d0647c86e9ccbda4
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-02-20 14:14:27 +0900

    add comment admin

--- DjangoServer/board/admin.py
+++ DjangoServer/board/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from board.models import Board
+from board.models import Board, Comment
+
 
 class BoardAdmin(admin.ModelAdmin):
     list_display = ('id', 'owner', 'name', 'title', 'date')
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'owner', 'name', 'date', 'content', 'board_id']
+
 admin.site.register(Board, BoardAdmin)
+admin.site.register(Comment, CommentAdmin)
