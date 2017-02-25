216e211451048708a424a27666dd62de802cc9f1
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-02-25 18:36:36 +0900

    divide board category url

--- DjangoServer/DjangoServer/urls.py
+++ DjangoServer/DjangoServer/urls.py
@@ -4,17 +4,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 
-from board.views import BoardViewSet, CommentViewset
-from users.views import UserViewSet, ProfileViewSet
-
 router = DefaultRouter()
-router.register(r'users_List', UserViewSet)
-router.register(r'users_Profiles', ProfileViewSet)
-router.register(r'board_List', BoardViewSet)
-router.register(r'board_Comments', CommentViewset)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^board/', include('board.urls')),
     url(r'^users/', include('users.urls')),
--- DjangoServer/board/urls.py
+++ DjangoServer/board/urls.py
@@ -2,12 +2,18 @@ from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
 from board import views
+from board.views import WriteAPIView
 
 router = DefaultRouter()
-router.register(r'list', views.BoardViewSet)
+router.register(r'web', views.WebViewSet)
+router.register(r'mobile', views.MobileViewSet)
+router.register(r'bigdata', views.BigDataViewSet)
+router.register(r'design', views.DesignViewSet)
+router.register(r'etc', views.ETCViewSet)
 router.register(r'comments', views.CommentViewset)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^write/', WriteAPIView.as_view(), name='register'),
 ]
