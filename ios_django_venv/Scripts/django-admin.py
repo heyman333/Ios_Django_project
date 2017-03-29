215033b858c524e457d7244e4568cdd60c8631b2
HanYoungsoo <gglife333@gmail.com>
2017-03-30 04:22:47 +0900

    delete file

--- ios_django_venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\kimmingoo\desktop\ios_django\ios_django_venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
